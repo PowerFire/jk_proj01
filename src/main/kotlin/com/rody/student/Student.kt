fb0dd981550ff686a4864d6d501e1dd4d7a95a57
Rody <rody0335@hotmail.com>
2019-05-10 00:07:00 +0800

    Kotlin的判斷式,寫出更實用的方法

--- src/main/kotlin/com/rody/student/Student.kt
+++ src/main/kotlin/com/rody/student/Student.kt
@@ -26,11 +26,41 @@ private fun userInput() {
 
 class Student(var name: String, var english:Int, var math:Int) {
     fun print() {
-        println(name+"\t"+english+"\t"+math+"" +
+        print(name+"\t"+english+"\t"+math+"" +
                 "\t"+ getAverage() +"\t"+ if (getAverage()>=60) "PASS" else "FAILED")
-
+        println("\t"+grading())
     }
 
+    fun grading()= when(getAverage()){
+                    in 90..100 -> 'A'
+                    in 80..89  -> 'B'
+                    in 70..79  -> 'C'
+                    in 60..69  -> 'D'
+                    else -> 'F'
+                }
+    /*
+    fun grading():Char {
+        var grading: Char ='F'
+        grading= when(getAverage()){
+                    in 90..100 -> 'A'
+                    in 80..89  -> 'B'
+                    in 70..79  -> 'C'
+                    in 60..69  -> 'D'
+                    else -> 'F'
+                 }
+        return grading;
+    }
+
+     */
+
+    fun highest()=if(english>math) {
+                    println("English")
+                    english
+                } else {
+                    println("Math")
+                    math
+                }
+    /*
     fun highest():Int{
         var max = if(english>math) {
             println("English")
@@ -42,8 +72,15 @@ class Student(var name: String, var english:Int, var math:Int) {
         return max
     }
 
+     */
+
+    fun getAverage()=(english+math)/2
+    /*
     fun getAverage():Int {
         return (english+math)/2
 
     }
+
+     */
+
 }

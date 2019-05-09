64f0dd26056ef3432ffec6e533fbfe9b183edf36
Rody <rody0335@hotmail.com>
2019-05-09 23:50:17 +0800

    Java-switch 方法

--- src/main/java/com/rody/student/Student.java
+++ src/main/java/com/rody/student/Student.java
@@ -23,13 +23,36 @@ public class Student {
 
     public void print() {
         System.out.print(name+"\t"+math+"\t"+english+
-                "\t"+getAverage());
+                "\t"+ getAverage()+ "\t"+((getAverage() > 60) ? "PASS" :"Failed"));
+
+        char grading='F';
+        switch (getAverage()/10) {
+            case 10:
+            case 9:
+                grading='A';
+                break;
+            case 8:
+                grading='B';
+                break;
+            case 7:
+                grading='C';
+                break;
+            case 6:
+                grading='D';
+                break;
+            default:
+                break;
+
+        }
+        System.out.println("\t"+ grading);
+        /*
         if (getAverage()>=60){
             System.out.println("\tPASS");
         }
         else {
             System.out.println("\tFailed");
         }
+         */
     }
 
     public int highest(){
